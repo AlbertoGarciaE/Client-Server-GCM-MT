9c2eaf62eb174a95407bb0e6caf819b0f27e8c30
Alberto García <garcia.estevez.alberto@gmail.com>
2015-07-05 11:38:34 +0200

    Created Project GCMServerTFM in STS Workspace. This project contains the src for the GCM testing tool developed for my final master thesis.

---
+++ GCMServer/src/main/java/gcmserver/GcmServerApplication.java
@@ -0,0 +1,12 @@
+package gcmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcmServerApplication.class, args);
+    }
+}
